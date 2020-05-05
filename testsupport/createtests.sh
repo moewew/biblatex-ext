f92d97b58b404c6511b265be5e16aa3f36462cac
moewew <mwemheu+github@gmail.com>
2020-05-05 07:55:27 +0200

    Test autocite and multicite

--- testsupport/createtests.sh
+++ testsupport/createtests.sh
@@ -4,6 +4,6 @@ for citestylefile in ./*.cbx ; do
   stylebasename=$(basename -- "$citestylefile")
   stylename="${stylebasename%.*}"
   echo "$stylename"
-  cp "./testfiles/template-boxed-style.lvtemplate" "./testfiles/boxed-style-$stylename.lvt"
-  sed -i "s/<style>/$stylename/g" "./testfiles/boxed-style-$stylename.lvt"
+  cp "./testfiles/template-boxed-style.lvtemplate" "./testfiles/style-$stylename.lvt"
+  sed -i "s/<style>/$stylename/g" "./testfiles/style-$stylename.lvt"
 done
