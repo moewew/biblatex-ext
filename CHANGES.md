9caf377558173cc483bcf0483412881e6262b3c9
moewew <mwemheu+git@posteo.de>
2025-03-31 20:58:07 +0200

    Release v0.19

--- CHANGES.md
+++ CHANGES.md
@@ -1,8 +1,8 @@
 # Changelog
 
-## Unreleased
+## Version 0.19 (2025-03-31)
 
-See <https://github.com/moewew/biblatex-ext/compare/v0.18...HEAD>
+See <https://github.com/moewew/biblatex-ext/compare/v0.18...v0.19>
 
 ### Changed
 - Call `\@doendpe` in `\printbibtabular`
--- blxextdoiapi.lua
+++ blxextdoiapi.lua
@@ -2,8 +2,8 @@ blxextdoiapi = blxextdoiapi or {}
 
 local blxextdoiapi_module = {
   name          = "blxextdoiapi",
-  version       = "v0.18",
-  date          = "2024/03/24",
+  version       = "v0.19",
+  date          = "2025/03/31",
   description   = "Obtain open access information from DOI via unpaywall.org",
   author        = "Moritz Wemheuer",
   license       = "LPPL v1.3c",
